e6c9bf1c2cac9d0c1afe3efdba720d126d186905
mcastroa7 <manucastroa7@gmail.com>
2025-12-03 16:19:00 -0300

    cambiando env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: 'http://localhost:5173', // Tu frontend
+    // Permitimos localhost (para tus pruebas) Y cualquier otro origen (*) por ahora
+    // Cuando tengas el dominio de Hostinger, cambiarás el '*' por 'https://agentsport.com'
+    origin: ['http://localhost:5173', '*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  // Esto está perfecto, Railway inyectará su propio PORT
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
