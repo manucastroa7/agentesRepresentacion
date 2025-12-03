c56733cf284b93b276249ad13af8956b6d341fad
mcastroa7 <manucastroa7@gmail.com>
2025-12-03 18:17:32 -0300

    cambiando smtp a mail

--- backend/src/modules/mail/mail.service.ts
+++ backend/src/modules/mail/mail.service.ts
@@ -8,12 +8,12 @@ export class MailService {
 
     constructor(private configService: ConfigService) {
         this.transporter = nodemailer.createTransport({
-            host: this.configService.get<string>('SMTP_HOST'),
-            port: this.configService.get<number>('SMTP_PORT'),
+            host: this.configService.get<string>('MAIL_HOST'),
+            port: this.configService.get<number>('MAIL_PORT'),
             secure: false, // true for 465, false for other ports
             auth: {
-                user: this.configService.get<string>('SMTP_USER'),
-                pass: this.configService.get<string>('SMTP_PASS'),
+                user: this.configService.get<string>('MAIL_USER'),
+                pass: this.configService.get<string>('MAIL_PASS'),
             },
         });
     }
